d7634bbbe39921056bd73de297889ac995186dbf
CodeLmm <2506731793@qq.com>
2019-11-17 11:18:14 +0800

    t

--- car-shop-controller/src/main/java/cn/jzdy/test/TestController.java
+++ car-shop-controller/src/main/java/cn/jzdy/test/TestController.java
@@ -3,21 +3,18 @@ package cn.jzdy.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import cn.jzdy.annotation.aop.CheckOnLine;
 import cn.jzdy.service.RedisService;
 
 @RestController
 public class TestController {
-	@Autowired
-	private TestDao testDao;
-	@GetMapping("testMybatis")
-	public Object testMybatis() {
-		//int i =1/0;
-		return testDao.testMybatis();
-	}
+//	@Autowired
+//	private TestDao testDao;
+//	@GetMapping("testMybatis")
+//	public Object testMybatis() {
+//		//int i =1/0;
+//		return testDao.testMybatis();
+//	}
 	@Autowired
 	private RedisService redisService;
 	@GetMapping("testRedis")
@@ -25,10 +22,11 @@ public class TestController {
 		redisService.set("username", name);
 		return redisService.get("username"); 
 	}
-	@CheckOnLine
-	@PostMapping("login")
+	
+	@GetMapping("login")
 	public Object checkOnlineTest() {
 		return null;
 	}
+	
 	
 }
